59f591478b2472ecf95f9f5d3cadefd7e0f9af9f
tommy.xiong <tommy.xiong@temu.com>
2025-02-02 04:01:23 +0800

    feat: next config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // webpack: (config) => {
-  //   config.module.rules.push({
-  //     test: /\.json$/,
-  //     type: "json",
-  //   });
-  //   return config;
-  // },
+  output: "export",
 };
 
 export default nextConfig;
